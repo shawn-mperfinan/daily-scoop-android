3fb9ed6ba593396684590e1b5b2c4b80aaa79d52
“shawn-mperfinan” <shawn.mperfinan@gmail.com>
2024-01-18 11:58:16 +0800

    Enhancement: disabled lint on release build
    - set checkReleaseBuilds to false in app build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -96,6 +96,10 @@ android {
         }
     }
 
+    lint {
+        checkReleaseBuilds = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
